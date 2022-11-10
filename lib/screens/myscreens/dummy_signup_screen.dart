4a3e0b227547dbca30ec25234bf5ca934545e356
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-11-10 12:56:44 +0200

    my commit mesage

--- lib/screens/myscreens/dummy_signup_screen.dart
+++ lib/screens/myscreens/dummy_signup_screen.dart
@@ -129,146 +129,152 @@ class _DummySignUpScreenState extends State<DummySignUpScreen> {
 
   @override 
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        child: Column(
-          children: [
-            Text(
-                'shopping 🛍',
-                style: GoogleFonts.lobster(
-                  textStyle: const TextStyle(
-                    fontSize: 30,
-                    //fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 2,),
+              Text(
+                  "Hello World",
+                  style: TextStyle(
+                    fontFamily: "Countryside",
+                    //fontFamily: "Signamaestro",
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            const SizedBox(height: 10,),
-            //get or capture image
-
-            Stack(
-                children: [
-                  imgXFile != null
-                    ? CircleAvatar(
-                      radius: MediaQuery.of(context).size.height * 0.10,
-                      backgroundImage: FileImage(File(imgXFile!.path),),
-                    )
-                  : const CircleAvatar(
-                    backgroundColor: Color.fromARGB(255, 241, 214, 214),
-                    radius: 54,
-                    child: Icon(
-                      Icons.person,
-                      size: 90,
-                      color: Colors.blueGrey,
+              const SizedBox(height: 10,),
+              //get or capture image
+      
+              Stack(
+                  children: [
+                    imgXFile != null
+                      ? CircleAvatar(
+                        radius: MediaQuery.of(context).size.height * 0.10,
+                        backgroundImage: FileImage(File(imgXFile!.path),),
+                      )
+                    : const CircleAvatar(
+                      backgroundColor: Color.fromARGB(255, 241, 214, 214),
+                      radius: 54,
+                      child: Icon(
+                        Icons.person,
+                        size: 90,
+                        color: Colors.blueGrey,
+                      ),
+                      /*backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkH5w8otgrD11yjJwk6HW7ENe9E7rpj7qd5Q&usqp=CAU",
+                      ),*/
                     ),
-                    /*backgroundImage: NetworkImage(
-                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkH5w8otgrD11yjJwk6HW7ENe9E7rpj7qd5Q&usqp=CAU",
-                    ),*/
-                  ),
-                  Positioned(
-                    bottom: -10,
-                    left: 70,
-                    child: IconButton(
-                      onPressed: getImageFromGallery,
-                      icon: const Icon(
-                        Icons.add_a_photo,
-                        color: Colors.lightBlue,
+                    Positioned(
+                      bottom: -10,
+                      left: 70,
+                      child: IconButton(
+                        onPressed: getImageFromGallery,
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                          color: Colors.lightBlue,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-            ),
-          
-
-
-            /*GestureDetector(
-              onTap: () {
-                //pickImage();
-                getImageFromGallery();
-              },
-              child: CircleAvatar(
-                radius: MediaQuery.of(context).size.width *0.20,
-                backgroundColor: const Color.fromARGB(255, 234, 233, 233),
-                backgroundImage: imgXFile == null 
-                    ? null 
-                    : FileImage(
-                      File(imgXFile!.path),
+                  ],
+              ),
+            
+      
+      
+              /*GestureDetector(
+                onTap: () {
+                  //pickImage();
+                  getImageFromGallery();
+                },
+                child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width *0.20,
+                  backgroundColor: const Color.fromARGB(255, 234, 233, 233),
+                  backgroundImage: imgXFile == null 
+                      ? null 
+                      : FileImage(
+                        File(imgXFile!.path),
+                      ),
+                  child: imgXFile == null 
+                      ? Icon(
+                        Icons.add_photo_alternate,
+                        color: const Color.fromARGB(255, 46, 83, 101),
+                        size: MediaQuery.of(context).size.width * 0.20,
+                      ) : null,
+                ),
+              ), */
+              const SizedBox(height: 5,),
+              //inputs form fields
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    //name
+                    CustomTextField(
+                      textEditingController: nameTextEditingController,
+                      iconData: Icons.person,
+                      hintText: 'Name',
+                      textInputType: TextInputType.text,
+                      isObscre: false,
+                      enabled: true,
                     ),
-                child: imgXFile == null 
-                    ? Icon(
-                      Icons.add_photo_alternate,
-                      color: const Color.fromARGB(255, 46, 83, 101),
-                      size: MediaQuery.of(context).size.width * 0.20,
-                    ) : null,
-              ),
-            ), */
-            const SizedBox(height: 5,),
-            //inputs form fields
-            Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  //name
-                  CustomTextField(
-                    textEditingController: nameTextEditingController,
-                    iconData: Icons.person,
-                    hintText: 'Name',
-                    textInputType: TextInputType.text,
-                    isObscre: false,
-                    enabled: true,
-                  ),
-                  //email
-                  CustomTextField(
-                    textEditingController: emailTextEditingController,
-                    iconData: Icons.email,
-                    hintText: 'Email',
-                    textInputType: TextInputType.emailAddress,
-                    isObscre: false,
-                    enabled: true,
-                  ),
-                  //password
-                  CustomTextField(
-                    textEditingController: passwordTextEditingController,
-                    iconData: Icons.lock_outline,
-                    hintText: 'Password',
-                    textInputType: TextInputType.text,
-                    isObscre: true,
-                    enabled: true,
-                  ),
-                  //confirmPassword
-                  CustomTextField(
-                    textEditingController: confirmPasswordTextEditingController,
-                    iconData: Icons.lock_outline,
-                    hintText: 'Confirm Password',
-                    textInputType: TextInputType.text,
-                    isObscre: true,
-                    enabled: true,
-                  ),
-
-                  const SizedBox(height: 10,),
-                ],
-              ),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 50,
-                  vertical: 12,
+                    //email
+                    CustomTextField(
+                      textEditingController: emailTextEditingController,
+                      iconData: Icons.email,
+                      hintText: 'Email',
+                      textInputType: TextInputType.emailAddress,
+                      isObscre: false,
+                      enabled: true,
+                    ),
+                    //password
+                    CustomTextField(
+                      textEditingController: passwordTextEditingController,
+                      iconData: Icons.lock_outline,
+                      hintText: 'Password',
+                      textInputType: TextInputType.text,
+                      isObscre: true,
+                      enabled: true,
+                    ),
+                    //confirmPassword
+                    CustomTextField(
+                      textEditingController: confirmPasswordTextEditingController,
+                      iconData: Icons.lock_outline,
+                      hintText: 'Confirm Password',
+                      textInputType: TextInputType.text,
+                      isObscre: true,
+                      enabled: true,
+                    ),
+      
+                    const SizedBox(height: 10,),
+                  ],
                 ),
               ),
-              onPressed: () {
-                formValidation();
-              }, 
-              child: Text(
-                'Sign Up',
-                style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12,
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 12,
+                  ),
+                ),
+                onPressed: () {
+                  formValidation();
+                }, 
+                child: Text(
+                  'Sign Up',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30,),
-          ],
+              const SizedBox(height: 30,),
+            ],
+          ),
         ),
       ),
     );
